edda7cd9db31366a80213e8edcc1d6ed7e4127be
kama casta <kamacasta983@gmail.com>
2021-07-02 14:47:06 -0700

    Packages installed consts created, changed array method given from starter code

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,13 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+
+    // Questions begin using multi-line strings
+    return
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
